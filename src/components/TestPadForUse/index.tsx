22ab5f0ad3817c1c82f515c925a4050b2426137d
QuantumNg <wujs16@gmail.com>
2022-06-13 09:17:39 +0800

    feat: add question and test line

--- src/components/TestPadForUse/index.tsx
+++ src/components/TestPadForUse/index.tsx
@@ -1,9 +1,9 @@
 import { FC, useEffect, useRef, useState } from 'react';
-import { Toast } from 'antd-mobile';
-import { motion, AnimatePresence, useAnimation, useDragControls, PanInfo } from "framer-motion";
+import { Toast, Dialog } from 'antd-mobile';
+import { motion, AnimatePresence, useAnimation, useDragControls, PanInfo, isDragActive } from "framer-motion";
 import cn from 'classnames';
 
-import { Step } from '../constant';
+import { Step, getQuestion } from '../constant';
 
 import ModelSample from '../ModelSample';
 import ModelAntigen from '../ModelAntigen';
@@ -15,16 +15,11 @@ import './index.scss';
 
 const SampleBottle: FC<any> = (props) => {
     const bottleRef = useRef(null);
-    const position = useRef({ x: 0, y: 0 });
     // const dragControls = useDragControls();
 
-    const handleOnDrag = (e: MouseEvent | TouchEvent | PointerEvent, info: PanInfo) => {
-        position.current = { x: info.point.x, y: info.point.y };
-    }
-
     const handleDragEnd = (e: MouseEvent | TouchEvent | PointerEvent, info: PanInfo, hasB1: boolean) => {
-        if (info.point.x > 35 && info.point.x < 65 && info.point.y > 600 && info.point.y < 650) {
-            console.log('right place');
+        const { x, y, width, height } = props.dropPosition;
+        if (info.point.x > x && info.point.x < x + width && info.point.y > y && info.point.y < y + height) {
             props.onDragEnd(Date.now(), hasB1);
             return;
         }
@@ -32,7 +27,6 @@ const SampleBottle: FC<any> = (props) => {
     };
 
     return <>
-        <div>x: {position.current.x} {' '} y:{position.current.y}</div>
         <div className='bottle bottle-1'>
             <motion.div
                 drag
@@ -40,7 +34,6 @@ const SampleBottle: FC<any> = (props) => {
                     scale: 1.2
                 }}
                 dragSnapToOrigin
-                onDrag={handleOnDrag}
                 onDragEnd={(e, i) => { handleDragEnd(e, i, true) }}
             >
                 <div className="bottle-head" ref={bottleRef}>
@@ -61,7 +54,6 @@ const SampleBottle: FC<any> = (props) => {
                     scale: 1.2
                 }}
                 dragSnapToOrigin
-                onDrag={handleOnDrag}
                 onDragEnd={(e, i) => { handleDragEnd(e, i, false) }}
             >
                 <div className="bottle-head" ref={bottleRef}>
@@ -137,34 +129,94 @@ const GroupSecondaryAntibody: FC<any> = (props) => {
     </>
 };
 
-const TestPad: FC<any> = ({ dropFlag, isWithB1, useStep } ) => {
+const GroupAntigenWithAntibody: FC<any> = (props) => {
+    return <>
+        {
+            props.isWithB1 ? <div className="antigen-antibody-group with-b1">
+                <div>
+                    <ModelAntibody {...props} />
+                    <ModelAntigen {...props} />
+                </div>
+            </div> : <div className="antigen-antibody-group without-b1">
+                <ModelAntibody {...props} />
+                <ModelAntigen {...props} />
+                <ModelAntibody {...props} />
+            </div>
+        }
+    </>
+}
+
+const GroupAntigenWithPrimaryAntibody: FC<any> = (props) => {
+
+    useEffect(() => {
+        console.log('GroupAntigenWithPrimaryAntibody', props.isDragging);
+    }, [props.isDragging]);
+
+    return <>
+        {
+            props.isWithB1 ? <div className="antigen-primary-antibody-group with-b1">
+                <GroupAntigenWithAntibody {...props} />
+            </div> : <div className="antigen-primary-antibody-group without-b1">
+                <ModelPrimaryAntibody {...props} />
+                <ModelAntigen {...props} />
+                <ModelPrimaryAntibody {...props} />
+            </div>
+        }
+    </>
+}
+
+
+
+const TestPad: FC<any> = ({ dropFlag, isWithB1, useStep, setPosion } ) => {
     const [ step, setStep ] = useStep;
+    const positionRef = useRef(null);
     // @ts-ignore
     const [limite, setLimite] = useState(limiteMap[step]);
+    const [rightCount, setRightCount] = useState(0);
+    const [ isTLineActive, setTLineActive ] = useState(false);
+    const [ isCLineActive, setCLineActive ] = useState(false);
     const dripAnimation = useAnimation();
     const sampleAnimation = useAnimation();
     const antibodyAnimation = useAnimation();
 
     // @ts-ignore
-    const handleDragEnd = (e, i) => {
+    const handleDragEnd = async(e, i) => {
+
+        console.log('handleDragEnd', step);
         if (step >= Step.Five) {
-            antibodyAnimation.start({
+            await antibodyAnimation.start({
                 x: 400,
                 opacity: 0,
-            })
+            });
             return;
         };
-        console.log('TestPad handleDragEnd', i.point);
         // @ts-ignore
         if (i.point.x > distanceMap[step]) {
+            if (step === Step.Four || step === Step.Three) {
+                const { question, options, answer } = getQuestion(step, isWithB1);
+                const [ confirmText, cancelText ] = options;
+                const res = await Dialog.confirm({ title: '请回答', content: question, cancelText, confirmText });
+                if (res === answer) {
+                    setRightCount(rightCount + 1);
+                };
+                if (step === Step.Four) {
+                    setCLineActive(true);
+                }
+                if (step === Step.Three && !isWithB1) {
+                    setTLineActive(true);
+                }
+            }
             console.log('right place', step);
             // @ts-ignore
             setLimite(limiteMap[step]);
             setStep(step + 1);
         }
+
     }
 
     const handleDropStart = async() => {
+        setCLineActive(false);
+        setTLineActive(false);
         await dripAnimation.start({
             y: [50, 80, 100],
             opacity: [0, 1, 0],
@@ -184,9 +236,18 @@ const TestPad: FC<any> = ({ dropFlag, isWithB1, useStep } ) => {
         // @ts-ignore
         console.log('listen step', step, limite, distanceMap[step]);
     }, [step]);
+
+    useEffect(() => {
+        // @ts-ignore
+        positionRef.current && setPosion(positionRef.current.getBoundingClientRect());
+    }, [positionRef]);
     
-    return <div className="testpad-wrapper">
-        <div className="drop-position drop-position-1">
+    return <div className={cn("testpad-wrapper", {
+        'tline-active': isTLineActive,
+        'cline-active': isCLineActive,
+    })}>
+        <div className="answer-zone">答对{rightCount}题/总数4题</div>
+        <div className="drop-position drop-position-1" ref={positionRef}>
         <motion.div animate={dripAnimation} initial={{opacity: 0}}>
             <div className="drip"></div>
         </motion.div>
@@ -202,8 +263,8 @@ const TestPad: FC<any> = ({ dropFlag, isWithB1, useStep } ) => {
                 {
                     step > Step.One && <motion.div drag='x' animate={antibodyAnimation} dragConstraints={{ left: 0, right: limite }} onDragEnd={handleDragEnd}>
                         { step === Step.Two ? <ModelAntigen isWithB1={isWithB1} /> : null }
-                        { step === Step.Three ? <ModelAntibody isWithB1={isWithB1} /> : null }
-                        { step === Step.Four ? <ModelPrimaryAntibody isWithB1={isWithB1} /> : null }
+                        { step === Step.Three ? <GroupAntigenWithAntibody isWithB1={isWithB1} /> : null }
+                        { step === Step.Four ? <GroupAntigenWithPrimaryAntibody isWithB1={isWithB1} isDragging={isDragActive()} /> : null }
                         { step >= Step.Five ? <ModelSecondaryAntibody isWithB1={isWithB1} /> : null }
                     </motion.div>
                 }
@@ -224,6 +285,7 @@ const TestPadForUse: FC = (props) => {
     const [sampleWithB1, setSampleWithB1] = useState(false);
     const [step, setStep] = useState(Step.One);
     const [dropFlag, refreshDropFlag] = useState(0);
+    const [position, setPosion] = useState(null);
 
     const handleBottleDrag = (flag: number, withB1: boolean) => {
         setStep(Step.One);
@@ -232,8 +294,8 @@ const TestPadForUse: FC = (props) => {
     }
 
     return <div className='testpad-for-use'>
-        <SampleBottle onDragEnd={handleBottleDrag}  />
-        <TestPad dropFlag={dropFlag} isWithB1={sampleWithB1} useStep={[step, setStep]} />
+        <SampleBottle onDragEnd={handleBottleDrag} dropPosition={position} />
+        <TestPad dropFlag={dropFlag} isWithB1={sampleWithB1} useStep={[step, setStep]} setPosion={setPosion} />
     </div>
 };
 
--- src/components/constant.ts
+++ src/components/constant.ts
@@ -5,3 +5,35 @@ export const enum Step {
     Four,
     Five,
 }
+
+
+const questionList1 = {
+    [Step.Three]: {
+        question: '若样品中含有黄曲霉毒素B1，T线是否显色？',
+        options: [ '显色', '不显色' ],
+        answer: false
+    },
+    [Step.Four]: {
+        question: '若样品中含有黄曲霉毒素B1，C线是否显色？',
+        options: [ '显色', '不显色' ],
+        answer: true
+    },
+}
+
+const questionList2 = {
+    [Step.Three]:         {
+        question: '若样品中不含黄曲霉毒素B1，T线是否显色？',
+        options: [ '显色', '不显色' ],
+        answer: true
+    },
+
+    [Step.Four]: {
+        question: '若样品中不含黄曲霉毒素B1，C线是否显色？',
+        options: [ '显色', '不显色' ],
+        answer: true
+    },
+}
+export const getQuestion = (step: number, hasB1: boolean) => {
+    // @ts-ignore
+    return hasB1 ? questionList1[step] : questionList2[step];
+}
